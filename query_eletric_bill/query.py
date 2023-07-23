28980f5e6298d29ba14680cd339da9722a653dab
yzhzy12321 <103085079+yzhzy12321@users.noreply.github.com>
2023-07-23 20:06:00 +0800

    完善模块功能，现在支持通过传入参数获取不同宿舍的电费信息

--- query_eletric_bill/query.py
+++ query_eletric_bill/query.py
@@ -2,7 +2,7 @@ import requests as re
 
 def get_html_data(sysid:int,areaid:int,districtid:int,buildid:int,floorid:int,roomid:int,filename='1.html'):
   s=re.Session()
-  params={'sysid':'2','areaid':'2','districtid':'1','buildid':'2','floorid':'15','roomid':'1050'}#要查找的宿舍号
+  params={'sysid':str(sysid),'areaid':str(areaid),'districtid':str(districtid),'buildid':str(buildid),'floorid':str(floorid),'roomid':str(roomid)}#要查找的宿舍号
   s.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1788.0"})
   res=s.get(url="https://yktepay.lixin.edu.cn/ykt/h5/eleresult?",params=params)#获取电费剩余
   
